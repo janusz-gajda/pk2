cf54674f17eb7d8ac63a6fded0281a6b82517e7b
Noob Noobowski <noob7007@gmail.com>
2020-07-16 12:21:50 +0200

    16.07.2020 - soduku solver - 1st attempt

--- projekt/test.c
+++ projekt/test.c
@@ -1,5 +1,7 @@
 /*  checkthis.c  */
-
+//TODO:
+// Set array = 0 when error
+// Think, how to act, when error further down the line
 #include <ncurses.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,6 +9,113 @@
 #include <time.h>
 #include <signal.h>
 
+void print_array_ncurses(WINDOW *win,int *array, int *positions_y, int *positions_x){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            mvwprintw (win,*(positions_y + ( i * 8) + j), *(positions_x + ( i * 8) + j), "%d", *(array + ( i * 8) + j));  
+        }    
+    }
+}
+
+int sudoku_solver(WINDOW *win,int *array, int *positions_y, int *positions_x, int y, int x){
+    // if is given as hint, let's go to another one
+    if(*(array + ( y * 8) + x) != 0){
+        //if is end of array
+        if(x == 8 && y == 8){
+            return 1;
+        } // if end of row 
+        else if(x == 8){
+            return sudoku_solver(win,array,positions_y,positions_x,y+1,0);
+        } else{
+            return sudoku_solver(win,array,positions_y,positions_x,y,x+1);
+        }
+    }else{
+        //if is empty
+        int all_good = 0;
+        int z = 1;
+        for(z = 1; z < 10; z++){
+            *(array + (y*8) + x) = z;
+            int err = 0;
+            //check, if error in col
+            for(int i = 0; i < 9; i++){
+                if(i == y){
+                    continue;
+                }
+                else if(*(array + (i*8) + x) == *(array + (y*8) + x)){
+                    err = 1; 
+                    break;
+                }
+            }
+            if(err == 1){
+                continue;
+            }
+            //check, if error in row
+            for(int i = 0; i < 9; i++){
+                if(i == x){
+                    continue;
+                }
+                else if(*(array + (y*8) + i) == *(array + (y*8) + x)){
+                    err = 1; 
+                    break;
+                }
+            }
+             if(err == 1){
+                continue;
+            }
+            //checki, if error in 3x3 grid
+
+            //determine, which 3x3 grid to compare against
+            int grid_y, grid_x; //address of first number in grid
+            if(y < 3)
+                grid_y = 0;
+            else if(y < 6)
+                grid_y = 3;
+            else
+                grid_y = 6;
+
+            if(x < 3)
+                grid_x = 0;
+            else if(y < 6)
+                grid_x = 3;
+            else
+                grid_x = 6;
+
+            int temp_y = grid_y, temp_x = grid_x
+            while(temp_y < grid_y+3){
+                while(temp_x < grid_x+3){
+                    if(*(array + (y*8) + x) == *(array + (grid_y*8) + grid_x)){
+                        err = 1;
+                        break;
+                    }
+                    temp_x++;
+                }
+                temp_x = grid_x;
+                temp_y++;
+            }
+            if(err == 1){
+                continue;
+            }
+            all_good = 1;         
+
+        }
+        if(all_good == 0){
+            return 0;
+        }else{
+            int temp;
+            if(x == 8 && y == 8){
+                return 1;
+            } // if end of row 
+            else if(x == 8){
+                temp = sudoku_solver(win,array,positions_y,positions_x,y+1,0);
+            } else{
+                temp = sudoku_solver(win,array,positions_y,positions_x,y,x+1);
+            }
+            if(temp == 1){
+                return 1;
+            }else
+        }
+    }
+}
 /*int calculate_x(int &x[9][9], int &y){
 int i_offsett = 1;
 int j_offsett = 1;
@@ -74,7 +183,7 @@ wrefresh(win);
 
 curs_set (0);
 int l = 0;
-while(l < 100000){
+while(l < 10000){
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
             c = rand() % 10;
